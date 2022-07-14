882971774a3ebccc9397ef41a0455ebcfc9d6cfc
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-14 17:09:37 -0500

    Implement game contuation after all cards selected

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function App() {
   function addSelection(selection) {
     if (!selections.includes(selection)) {
       setSelections([...selections, selection]);
+    } else if (selections.length === colorArray.length) {
+      // Allow the game to continue after all cards have been selected
+      setSelections([selection]);
     } else {
       setSelections([]);
       alert("You lost!");
