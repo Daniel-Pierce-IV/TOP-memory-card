e9d12d3820bf5fb0a718c7b68e980c3e8b8b1db8
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-14 02:36:34 -0500

    Extract colors to module

---
+++ src/colors.js
@@ -0,0 +1,20 @@
+const colors = [
+  "bg-[#FF0000]", // Red FF8500
+  // "bg-[#FF7400]", // Philippine Orange
+  "bg-[#FF8500]", // American Orange
+  // "bg-[#FFAA00]", // Chrome Yellow
+  "bg-[#FFCA00]", // Philippine Yellow
+  "bg-[#000000]", // Black
+  // "bg-[#FFD300]", // Cyber Yellow
+  "bg-[#FFFF00]", // Yellow
+  "bg-[#A2F300]", // Spring Bud
+  "bg-[#00DB00]", // Electric Green
+  "bg-[#00DFCF]", // Dark Turquoise
+  "bg-[#125AFF]", // Blue
+  // "bg-[#4B13FF]", // Han Purple
+  "bg-[#FFFFFF]", // White
+  "bg-[#A502FF]", // Vivid Violet
+  "bg-[#FF0090]", // Magenta
+];
+
+export default colors;
