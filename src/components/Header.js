acd68a1a083b11d84337123b104ce490a1de43ef
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-15 02:17:34 -0500

    Change title and instructions when game changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,31 @@ import Scoreboard from "./Scoreboard";
 export default function Header(props) {
   const { difficulty, updateDifficulty } = props;
 
+  const alphabetText = "Letter";
+  const colorText = "Color";
+  const fontText = "Font";
+
+  function currentText() {
+    switch (difficulty) {
+      case Difficulty.MEDIUM:
+        return colorText;
+      case Difficulty.HARD:
+        return fontText;
+      default:
+        return alphabetText;
+    }
+  }
+
   return (
     <header className="flex justify-between p-4 border-b-8 border-black">
       <div className="flex flex-col">
-        <span className="text-4xl">Color Memory</span>
-        <span>Score points by clicking on colors you havent clicked yet.</span>
+        <span className="text-4xl">{currentText()} Memory</span>
+        <span>
+          Score points by clicking on {currentText().toLowerCase()}s you havent
+          clicked yet.
+        </span>
         <span className="italic">
-          Color clicks reset once you click them all, so keep going!
+          {currentText()} clicks reset once you click them all, so keep going!
         </span>
       </div>
 
