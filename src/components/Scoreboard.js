436b706fd06f64805aafb92a423287ab0e6fff4d
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-14 22:59:11 -0500

    Clean up styling

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,10 +1,12 @@
 export default function Scoreboard(props) {
   const { score, best, hasReset } = props;
+  const scoreClasses =
+    "flex flex-col h-full justify-center items-center rounded-xl px-4 bg-black text-white";
 
   return (
     <div className="grid gap-4 grid-cols-2">
       <div
-        className={`flex flex-col h-full justify-center items-center rounded-xl px-4 bg-black text-white transition-transform duration-75 ${
+        className={`${scoreClasses} transition-transform duration-75 ${
           hasReset ? "bg-red-600 scale-[1.2]" : ""
         }`}
       >
@@ -12,7 +14,7 @@ export default function Scoreboard(props) {
         <span className="text-3xl leading-8 pb-0.5">{score}</span>
       </div>
 
-      <div className="flex flex-col h-full justify-center items-center rounded-xl px-4 bg-black text-white">
+      <div className={scoreClasses}>
         <span className="whitespace-nowrap text-lg">Best</span>
         <span className="text-3xl leading-8 pb-0.5">{best}</span>
       </div>
