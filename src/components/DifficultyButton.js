12421611c6d922bda98ebe1d875fbbe2fe7a43e5
Daniel Pierce <daniel.pierce.iv@gmail.com>
2022-07-15 01:54:37 -0500

    Fix active button styling

--- src/components/DifficultyButton.js
+++ src/components/DifficultyButton.js
@@ -1,11 +1,12 @@
 export default function DifficultyButton(props) {
   const { label, onClick, isActive } = props;
   const activeClasses = "border-blue-600 bg-blue-200";
+  const inactiveClasses = "border-gray-600 bg-white";
 
   return (
     <button
-      className={`grow basis-0 w-32 py-1 text-xl border-4 border-gray-600 bg-white hover:brightness-90 active:brightness-[0.8] ${
-        isActive ? activeClasses : ""
+      className={`w-32 py-1 text-xl border-4 hover:brightness-90 active:brightness-[0.8] ${
+        isActive ? activeClasses : inactiveClasses
       }`}
       type="button"
       onClick={onClick}
